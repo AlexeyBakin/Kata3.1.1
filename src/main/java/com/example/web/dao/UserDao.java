d2cf7337d8d8faaf00a45eebd6b1a4c80c084fbc
AlexeyBakin <blackacsel123@gmail.com>
2022-02-03 13:30:13 +0300

    Initial commit

---
+++ src/main/java/com/example/web/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.example.web.dao;
+
+import com.example.web.model.User;
+import java.util.List;
+
+public interface UserDao {
+    List<User> getAllUsers();
+
+    User getUserById(long id);
+
+    void add(User user);
+
+    void update(Long id, User newUser);
+
+    void delete(long id);
+}
